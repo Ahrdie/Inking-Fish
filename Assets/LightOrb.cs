39ffab5cca9d479df597faec5fbeaf55581dd672
Robert Deppe <contact@robertdeppe.de>
2019-10-26 00:40:30 +0200

    Add LightOrb Script and Prefab

---
+++ Assets/LightOrb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOrb : Collectible
+{
+    private Color _orbColor;
+    public Color OrbColor {
+        get {
+            return _orbColor;
+        }
+        set {
+            _orbColor = value;
+            GetComponent<Light>().color = value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<LightOrb>() != null){
+            Debug.Log("Other Orb found");
+        }
+    }
+}
